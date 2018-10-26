0597bb18bae886c155a64bdb9b47384c3234324e
mi0-1123 <si2.o.mjt@gmail.com>
2018-10-27 01:09:05 +0900

    fix and add 3ver.

--- serial3.py
+++ serial3.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import pygame.mixer
 import serial
 import time
@@ -18,35 +16,28 @@ if __name__ == '__main__':
 	mplist = glob.glob("soundfile/comp/*.mp3")
 	rollist = glob.glob("soundfile/roll/*.mp3")
 	pygame.mixer.init()
-#	print mplist.__class__.__name__
 	num = len(mplist)
 	rollen = len(rollist)
-#	print "listlen",num
 	count = 0
 	flg=False
 
 	while True:
-#		time.sleep(0.5)
 		try:
-			print "count",count
+			print('count',count)
 			count += 1
-#			itr = random.choice(range(num))
-#			myserial.write( "0,1,1,9")
 
 			strmsg=myserial.readline()
-			print "get",strmsg
-#			print "strmsg class is ",strmsg.__class__.__name__
+			print('get',strmsg)
 			if len(strmsg) is not 0:
-				vallist = strmsg.split(",")
+				vallist = strmsg.split(',')
 				vallistint=[]
 				for i in vallist:
 					tmp1 = i.replace('\n','')
 					tmp2 = tmp1.replace('\r','')
 					vallistint.append(int(tmp2))
 
-				print "castint",vallistint
+				print('castint',vallistint)
 
-#				if  vallistint[1] and vallistint[2]:
 				if  vallistint[2]:
 					if	not pygame.mixer.music.get_busy():
 							th = random.choice(range(rollen))
